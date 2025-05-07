bba2b7840d1c74e8ea6d380f89e5aeadfd803977
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-07 22:49:42 +0300

    Checking for anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [item for item in word_list if sorted(item) == sorted(self.word)]
+
+perfect_match = Anagram(["listen", "silent", "hippopotamus"])
